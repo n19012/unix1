2155ec3e634c62a03f71ab07ad5b84e450151e69
kowa <n19012@std.it-college.ac.jp>
2019-06-06 19:11:52 +0900

    6/4課題

---
+++ 0604/060401.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+file=$1
+if [ -f "$file" ]; then
+    du -b "$file"
+
+else
+    echo "通常のファイルではありません"
+
+fi
---
+++ 0604/060402.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in "$@"
+do
+    if [ -f "$file" ]; then
+        du -b "$file"
+    else
+        echo "${file}:通常のファイルではありません"
+    fi
+done
+
